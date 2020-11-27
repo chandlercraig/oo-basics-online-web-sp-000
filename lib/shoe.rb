123de67c896b27cac28cf3dfe1837ad41f8e7b39
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-27 19:37:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,14 +5,6 @@ class Shoe
     @brand = brand
   end
 
-  # def condition=(condition)
-  #   @condition = condition
-  # end
-  #
-  # def condition
-  #   @condition
-  # end
-
   def cobble
     puts "Your shoe is as good as new!"
 
